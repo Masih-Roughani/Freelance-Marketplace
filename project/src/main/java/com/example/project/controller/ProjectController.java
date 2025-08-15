96a54e416606c551a4f9c65c62fb5ae6cb447d2c
Masih-Roughani <masihroughani2005@gmail.com>
2025-08-15 17:53:40 +0330

    fix(controller, service, model): add assign process to the application

--- project/src/main/java/com/example/project/controller/ProjectController.java
+++ project/src/main/java/com/example/project/controller/ProjectController.java
@@ -6,10 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -22,10 +19,17 @@ public class ProjectController {
         this.projectService = projectService;
     }
 
-    @PostMapping
+    @PostMapping("create")
     @PreAuthorize("hasRole('EMPLOYER')")
     public ResponseEntity<?> createProject(@RequestBody ProjectCreationRequest projectCreationRequest, Authentication authentication) {
         projectService.createProject(projectCreationRequest, authentication);
         return ResponseEntity.status(HttpStatus.CREATED).body("Created project successfully");
+    }
+
+    @PostMapping("assign")
+    @PreAuthorize("hasRole('FREELANCER')")
+    public ResponseEntity<?> assign(@RequestParam UUID projectId, Authentication authentication) {
+        projectService.assignProject(projectId, authentication);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Assigned project successfully");
     }
 }
